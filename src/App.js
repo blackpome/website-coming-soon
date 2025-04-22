5af18fbe17b3f00f709f6f955d3bab237cbc945d
blackpome <jv@blackpome.com>
2025-04-22 12:04:55 +0530

    fix: coming soon text in mobile screens

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import arils from './assets/arils.png';
 function App() {
   return (
     <div className="text-center">
-      <header className="bg-[#121212] min-h-screen flex flex-col items-center justify-center text-white relative">
+      <header className="bg-[#121212] min-h-screen flex flex-col items-center justify-center text-white relative px-4">
         <img src={arils} className="absolute top-2 right-4" alt="arils" />
         
-        <p className="text-[148px] font-extralight font-sans whitespace-nowrap animate-scroll text-right w-full overflow-hidden">
+        <p className="font-extralight font-sans text-right w-full overflow-hidden whitespace-nowrap animate-scroll
+          text-[48px] sm:text-[72px] md:text-[100px] lg:text-[148px]">
           coming soon
         </p>
 
         <div className="fixed bottom-[50px] w-full flex justify-center items-center">
-          <img src={logo} className="inline-block" alt="logo" />
+          <img src={logo} className="inline-block max-w-[80%] sm:max-w-[300px]" alt="logo" />
         </div>
       </header>
     </div>
